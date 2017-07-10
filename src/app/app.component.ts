1bcb8b5f74857996688ca7a50eb64dab913f09e6
bogdancar <bcarpean@cleardestination.com>
2017-07-10 16:54:48 -0400

    initial add with the working demo app for libphonenumber-js on Angular 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { parse, format, asYouType } from 'libphonenumber-js';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  asYouType: any;
+  format: any;
+  parse: any;
+
+  ngOnInit() {
+    this.asYouType = new asYouType('US').input('2133734');
+    this.format = format('2133734253', 'US', 'International');
+    this.parse = parse('(0777) 844 822', 'RO');
+  }
+
 }
